b1ee519321e2f00403212b22ac5f9a423fc0ea2e
PiyushKumarAgarwal <92864991+PiyushKumarAgarwal@users.noreply.github.com>
2022-07-10 15:28:14 +0530

    Add files via upload

---
+++ portfolio.js
@@ -0,0 +1,20 @@
+function openthebox()
+{
+    document.getElementById("myNav").style.width="250px";
+}
+
+function closethebox()
+{
+    document.getElementById("myNav").style.width="0px";
+}
+
+function closethebox2()
+{
+    document.getElementById("loading-the-magic").style.display="none";
+}
+
+function magic()
+{
+    document.getElementById("loading-the-magic").style.display="block";
+    document.getElementById("loading-the-magic").style.position="absolute";
+}
